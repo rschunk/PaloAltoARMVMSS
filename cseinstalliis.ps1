be087dee68224f1a06d9fc0a8c8e06bc65543a3f
swiftsolves-msft <30509195+swiftsolves-msft@users.noreply.github.com>
2017-12-21 23:44:25 -0500

    Add files via upload

---
+++ cseinstalliis.ps1
@@ -0,0 +1,11 @@
+﻿$resourceGroup = "rg-azureelastictest"
+$vmName = "VM-Web2"
+$location = "East US2"
+
+
+# Install IIS
+$PublicSettings = '{"commandToExecute":"powershell Add-WindowsFeature Web-Server"}'
+
+Set-AzureRmVMExtension -ExtensionName "IIS" -ResourceGroupName $resourceGroup -VMName $vmName `
+  -Publisher "Microsoft.Compute" -ExtensionType "CustomScriptExtension" -TypeHandlerVersion 1.4 `
+  -SettingString $PublicSettings -Location $location

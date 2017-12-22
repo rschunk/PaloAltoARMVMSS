52eeef60fe25a981e2eb66b30300f34395b37d02
swiftsolves-msft <30509195+swiftsolves-msft@users.noreply.github.com>
2017-12-21 23:45:17 -0500

    Update cseinstalliis.ps1

--- cseinstalliis.ps1
+++ cseinstalliis.ps1
@@ -1,4 +1,16 @@
-﻿$resourceGroup = "rg-azureelastictest"
+﻿$resourceGroup = "xxxxx"
+$vmName = "VM-Web1"
+$location = "East US2"
+
+
+# Install IIS
+$PublicSettings = '{"commandToExecute":"powershell Add-WindowsFeature Web-Server"}'
+
+Set-AzureRmVMExtension -ExtensionName "IIS" -ResourceGroupName $resourceGroup -VMName $vmName `
+  -Publisher "Microsoft.Compute" -ExtensionType "CustomScriptExtension" -TypeHandlerVersion 1.4 `
+  -SettingString $PublicSettings -Location $location
+  
+$resourceGroup = "xxxxx"
 $vmName = "VM-Web2"
 $location = "East US2"
 
